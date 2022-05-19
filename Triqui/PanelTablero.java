f003eb5a9e89eefb1bce54e1de28f2bf3194c62c
AndresHL7 <105527032+AndresHL7@users.noreply.github.com>
2022-05-19 09:50:43 -0500

    Update PanelTablero.java

--- Triqui/PanelTablero.java
+++ Triqui/PanelTablero.java
@@ -1,15 +1,21 @@
 package com.usc.ui;
 
+import java.awt.GridLayout;
 import javax.swing.*;
 
 public class PanelTablero extends JPanel {
 
     JLabel etiquetaPrueba;
+    
+    JTextField casilla0, casilla1, casilla2, casilla3, casilla4, casilla5, casilla6, casilla7, casilla8, casilla9;
 
     public PanelTablero() {
 
-        etiquetaPrueba = new JLabel("Agui va el tablero");
+        etiquetaPrueba = new JLabel("Aqui va el tablero");
         this.add(etiquetaPrueba);
+        this.setLayout(new GridLayout (3,3));
+        
+       
 
     }
 }
