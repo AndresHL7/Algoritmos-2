4b59ccc9526e5d867ce32b74ac91bc27043aeaa5
AndresHL7 <105527032+AndresHL7@users.noreply.github.com>
2022-05-27 20:25:27 -0500

    Update VentanaPrincipal.java

--- Triqui/VentanaPrincipal.java
+++ Triqui/VentanaPrincipal.java
@@ -1,59 +1,42 @@
 package com.usc.ui;
 
-import java.awt.BorderLayout;
-import javax.swing.JFrame;
+    import java.awt.*;
+    import javax.swing.*;
 
+    public class VentanaPrincipal extends JFrame{
 
-public class VentanaPrincipal extends JFrame {
-    
-    
-    
-    
-    PanelLogo logoTriqui;
-    PanelTablero tableroTriqui;
-    PanelPuntaje puntajeTriqui;
-    
-    
- /*constructor para establecer las caracteristicas de la ventana */
- public VentanaPrincipal()
- { 
-     this.setTitle("Mi primera aplicación UI");
-     this.setBackground(java.awt.Color.yellow);
-     this.setSize(800, 600);
-     this.setResizable(false);
-     
-     
-     //Establecer diagramación para la Ventana Principal:
-     this.setLayout(new BorderLayout());
-     
-     //Instanciar y agregar panel del logo:
-     logoTriqui= new PanelLogo();
-     this.add(logoTriqui, BorderLayout.NORTH);
-     
-     //Instanciar y agregar panel del tablero:
-     tableroTriqui= new PanelTablero();
-     this.add(tableroTriqui, BorderLayout.CENTER);
-     
-     //Instanciar y agregar panel del logo:
-     puntajeTriqui= new PanelPuntaje();
-     this.add(puntajeTriqui, BorderLayout.EAST);
-     
-     
-    
-     
-     
-     
-     }
+        PanelLogo logoTriqui;
+        PanelTablero TableroTriqui;
+        PanelPuntaje PuntajeTriqui;
 
-    
-    public static void main (String[] args) {
-        
-        
-        VentanaPrincipal miVentana = new VentanaPrincipal();
-        miVentana.setVisible(true);
-    
-    
-  }
+        public static void main(String[] args){
 
-   
-}
+            VentanaPrincipal miVentana = new VentanaPrincipal();
+            miVentana.setVisible(true);
+
+        }
+
+        /* metodo constructor*/
+        public VentanaPrincipal(){
+            this.setTitle("Mi primera aplicacion con UI");
+            this.setBackground(Color.BLUE);
+            this.setSize(800,600);
+            this.setResizable(false);
+            
+            //Establecer diagramacion para la ventana principal:
+            this.setLayout(new BorderLayout());
+
+            //instanciar y agregar panel del logo:
+            logoTriqui = new PanelLogo();
+            this.add(logoTriqui, BorderLayout.NORTH);
+
+            //instanciar y agregar panel del triqu
+            TableroTriqui = new PanelTablero();
+            this.add(TableroTriqui, BorderLayout.CENTER);
+
+            //instanciar y agregar panel del puntaje
+            PuntajeTriqui = new PanelPuntaje();
+            this.add(PuntajeTriqui, BorderLayout.EAST);
+        }
+
+    }
